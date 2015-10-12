272959726548b300dbe5dd04f5f76a9f49e343fa
phundi <kennethkapundi1@gmail.com>
2015-10-12 09:02:09 +0200

    combined logic for running data consistency checks and that of pulling results in one def/method

--- app/controllers/validation_result_controller.rb
+++ app/controllers/validation_result_controller.rb
@@ -16,6 +16,7 @@ class ValidationResultController < ActionController::Base
     end_date = params[:end_date]
     resp = []
     (start_date..end_date).each do |date|
+      ValidationRule.data_consistency_checks(date)
       resp << ValidationRule.find_by_sql("SELECT validation_rules.id as rule_id, `desc` as description,
                                   COALESCE((SELECT failures FROM validation_results WHERE rule_id = validation_rules.id
                                   AND date_checked = '#{date}') ,0) AS failures, '#{date}' as date_checked from
