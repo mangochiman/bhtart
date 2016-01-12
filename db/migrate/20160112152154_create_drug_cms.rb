aa47512de5a2f385d67054d6f9d457e56283bc61
mangochiman <mangochiman@gmail.com>
2016-01-12 17:57:05 +0200

    Created DrugCms model

--- db/migrate/20160112152154_create_drug_cms.rb
+++ db/migrate/20160112152154_create_drug_cms.rb
@@ -1,9 +1,9 @@
 class CreateDrugCms < ActiveRecord::Migration
   def self.up
     create_table :drug_cms, :id => false  do |t|
-      t.integer :drug_inventory_id
-      t.string :name
-      t.string :code
+      t.integer :drug_inventory_id, :null => false
+      t.string :name, :null => false
+      t.string :code, :null => false
       t.integer :pack_size
       t.integer :voided, :default => 0, :limit => 1
       t.integer :voided_by , :limit => 11
