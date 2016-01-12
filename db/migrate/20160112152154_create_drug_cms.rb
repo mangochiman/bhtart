071e66288e737baf433dcd8410a596e60f12c640
mangochiman <mangochiman@gmail.com>
2016-01-12 20:43:43 +0200

    Added seed for CMS drugs

--- db/migrate/20160112152154_create_drug_cms.rb
+++ db/migrate/20160112152154_create_drug_cms.rb
@@ -1,6 +1,6 @@
 class CreateDrugCms < ActiveRecord::Migration
   def self.up
-    create_table :drug_cms, :id => false  do |t|
+    create_table :drug_cms, :id => false do |t|
       t.integer :drug_inventory_id, :null => false
       t.string :name, :null => false
       t.string :code, :null => false
@@ -11,9 +11,11 @@ class CreateDrugCms < ActiveRecord::Migration
       t.string :void_reason, :limit => 225
       t.timestamps
     end
+    execute "ALTER TABLE drug_cms ADD PRIMARY KEY (drug_inventory_id)"
   end
 
   def self.down
     drop_table :drug_cms
   end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,10 @@
 
 # Validation rules for cohort report
 require 'fastercsv'
+require 'spreadsheet'
 puts "Adding validation rules for cohort reports"
 FasterCSV.foreach('db/validation_rules.csv',
-                  :col_sep => ',', :headers => :first_row) do |row|
+  :col_sep => ',', :headers => :first_row) do |row|
 
   expr = row['expr'] || ''
   desc = row['desc'].to_s
@@ -24,3 +25,26 @@ FasterCSV.foreach('db/validation_rules.csv',
 
   end
 end
+
+def load_cms_drugs
+  cms_drugs = Spreadsheet.open "#{Rails.root}/script/cms.xls"
+  sheet1 = cms_drugs.worksheet 0
+
+  ActiveRecord::Base.transaction do
+    sheet1.each 2 do |row|
+      drug_name = row[0]
+      drug_code = row[1]
+      drug_inventory_id = row[2]
+      pack_size = drug_name.split(/[^\d]/).last
+      next if drug_inventory_id.blank?
+      drug_cms = DrugCms.new
+      drug_cms.drug_inventory_id = drug_inventory_id
+      drug_cms.name = drug_name
+      drug_cms.code = drug_code
+      drug_cms.pack_size = pack_size
+      drug_cms.save
+    end
+  end
+end
+
+load_cms_drugs
