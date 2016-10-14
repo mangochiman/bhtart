ef81c0738c4908c3102c07c5bda987853cbc6f3b
mangochiman <mangochiman@gmail.com>
2016-10-14 13:54:44 +0200

    Added :joins => [:encounter] to the named_scope

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -10,7 +10,7 @@ class Observation < ActiveRecord::Base
   belongs_to :answer_concept_name, :class_name => "ConceptName", :foreign_key => "value_coded_name_id", :conditions => {:voided => 0}
   has_many :concept_names, :through => :concept
 
-  named_scope :recent, lambda {|number| {:order => 'obs_datetime DESC,date_created DESC', :limit => number}}
+  named_scope :recent, lambda {|number| {:joins => [:encounter], :order => 'obs_datetime DESC,date_created DESC', :limit => number}}
   named_scope :before, lambda {|date| {:conditions => ["obs_datetime < ? ", date], :order => 'obs_datetime DESC, date_created DESC', :limit => 1}}
   named_scope :old, lambda {|number| {:order => 'obs_datetime ASC, date_created ASC', :limit => number}}
   named_scope :question, lambda {|concept|
