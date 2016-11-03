62c98735824afa8897ee8f37a53d2eacf00a4187
mangochiman <mangochiman@gmail.com>
2016-11-03 10:39:25 +0200

    Making sure HIV Clinic Registration encounter uses the same Stage Defining Conditions as HIV Staging encounter

--- app/controllers/encounters_controller.rb
+++ app/controllers/encounters_controller.rb
@@ -519,7 +519,7 @@ class EncountersController < GenericEncountersController
 				end
 			end
 
-			if (params[:encounter_type].upcase rescue '') == 'HIV_STAGING'
+			if ((params[:encounter_type].upcase rescue '') == 'HIV_STAGING') || (params[:encounter_type].upcase rescue '') == 'HIV_CLINIC_REGISTRATION'
 				#added current weight to use on HIV staging for infants
 				@current_weight = PatientService.get_patient_attribute_value(@patient,
           "current_weight")
