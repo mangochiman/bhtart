31408e0305448cdb99b4fdc6343dd43acbfb375d
phundi <kennethkapundi1@gmail.com>
2016-11-17 09:54:15 +0200

    file for views

--- db/couch_views.js
+++ db/couch_views.js
@@ -1,8 +1,14 @@
 {
 	"language": "javascript",
   "views": {
-    "by_rule_id_and_date": {
+    "by_rule_id": {
       "map": "function(doc) { emit(doc['rule_id']) }"
+    },
+		"by_date_checked": {
+      "map": "function(doc) { emit(doc['date_checked']) }"
+    },
+		"by_rule_id_and_date_checked": {
+      "map": "function(doc) { emit([doc['rule_id'], doc['date_checked']]) }"
     }
   }
 }
