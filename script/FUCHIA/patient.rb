2b14025f932792d0706552e699061e7056b4ddb5
Pachawo Bisani <phbisan@gmail.com>
2016-12-16 10:46:16 +0200

    Patient migration script

--- script/FUCHIA/patient.rb
+++ script/FUCHIA/patient.rb
@@ -1,27 +1,98 @@
 User.current = User.find_by_username('admin')
-Parent_path = '/home/mwatha/Desktop/msf/'
+Parent_path = '/home/pachawo/Documents/msf/'
 require 'fastercsv'
 
 def start
-   FasterCSV.foreach("#{Parent_path}/TbPatient.csv", :quote_char => '"', :col_sep =>',', :row_sep =>:auto) do |row|
+  FasterCSV.foreach("#{Parent_path}/TbPatient.csv", :quote_char => '"', :col_sep =>',', :row_sep =>:auto) do |row|
     names = row[9].split(' ')
-    given_name = names[0].titleize.squish rescue nil
-    family_name = names.last.titleize.squish rescue nil
-    given_name = given_name.gsub('*','') unless given_name.blank?
-    family_name = family_name.gsub('*','') unless family_name.blank?
+    given_name = nil ; middle_name = nil ; family_name = nil
+
+    (names || []).each_with_index do |name, i|
+      next if name.blank?
+      n = name.titleize.squish
+      n = n.gsub('*','Unknown') 
+      given_name = n if i == 0  
+      if i == 1 and names.length > 2
+	middle_name = n 
+      else
+        family_name = n if i == 1
+      end
+      family_name = n if i == 2
+    end
+
+    age_estimate = false
     gender = row[10].squish.to_i rescue 'Unknown'
-    
+    date_created = row[1] 
+    date_created = get_proper_date(date_created)
+    date_created = date_created.to_date.strftime("%Y-%m-%d 01:00:00") rescue Date.today.strftime("%Y-%m-%d 01:00:00")
+    age_estimate_date_created = row[14] 
+    date_of_death =get_proper_date(row[21]) unless row[21].blank?
+    is_dead = row[20] rescue nil
+    city_village = row[3] rescue nil
+    occupation = row[4] rescue nil
+    age = row[12]
+
+    if row[11].blank?
+      age_estimate = true
+      if !age_estimate_date_created.blank? and !age.blank?
+        age_estimate_date_created = get_proper_date(age_estimate_date_created)
+        dob = Date.new(age_estimate_date_created.to_date.year - age.to_i, 7, 1)
+     else
+       dob = "1900-01-01"
+     end
+    else
+      dob = get_proper_date(row[11])
+    end
+
     unless gender == 'Unknown'
       gender = gender == 0 ? 'M' : 'F'
     end
 
-    
+    person = Person.find(row[0].to_i) rescue nil
+    if person.blank?
+      puts "Creating:#{row[0]} ................ "
+      person = Person.new()
+      person.birthdate_estimated = age_estimate
+      person.birthdate = dob.to_date 
+      person.dead = is_dead.to_i
+      person.gender = gender unless gender == 'Unknown'
+      person.death_date = date_of_death.to_date unless date_of_death.blank?
+      person.date_created = date_created
+      person.person_id = row[0].to_i
+      person.save
 
-    puts ">>> #{given_name} #{family_name} (#{gender})"
+     PersonName.create(:given_name => given_name, :family_name => family_name, :middle_name => middle_name, 
+              :date_created => person.date_created, :person_id => person.id)
 
+     patient = Patient.new()
+     patient.patient_id = person.id
+     patient.date_created = person.date_created
+     patient.save
 
-   end  
+   end
+
+  end 
+ 
 end
 
+def get_proper_date (unfomatted_date)
+  unfomatted_date = unfomatted_date.split("/") unless unfomatted_date.blank?
+  year_of_birth = unfomatted_date[2].split(" ")
+  year_of_birth = year_of_birth[0]
+  current_year = Date.today.year.to_s
+
+  if year_of_birth.to_i > current_year[-2..-1].to_i
+    year = "19#{year_of_birth}"
+  else
+    year = "20#{year_of_birth}"
+  end
+
+  fomatted_date = "#{year}-#{unfomatted_date[0]}-#{unfomatted_date[1]}" 
+end
+
+def generate_date_of_birth(age, date_recorded)
+  age = age.to_i
+  date_of_birth = date_recorded.to_date - age.year
+end
 
 start
