10573ddf348b79ab439bb1668b5f46dd25304a7b
mwatha <mwathabwanali@gmail.com>
2017-01-11 14:33:04 +0200

    adding pediatric chart

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -269,7 +269,77 @@ class PatientsController < GenericPatientsController
     } rescue []
     label.print(2)
   end
-  
+ 
+  def area_chart_peds
+    patient = Patient.find(params[:patient_id])
+    person = patient.person
+    birthdate = person.birthdate.to_date rescue nil
+
+    concept_id = ConceptName.find_by_name("Weight (Kg)").concept_id
+    session_date = (session[:datetime].to_date rescue Date.today).strftime('%Y-%m-%d 23:59:59')
+    obs = []
+
+    weight_trail = {} ; current_date = (session_date.to_date - 2.year).to_date
+
+    weight_trail_data = ActiveRecord::Base.connection.select_all <<EOF
+    SELECT * FROM obs WHERE person_id = #{patient.id}
+    AND concept_id = #{concept_id} AND voided = 0 AND 
+    obs_datetime BETWEEN '#{(session_date.to_date - 2.year).strftime('%Y-%m-%d 00:00:00')}' 
+    AND '#{session_date}' ORDER BY obs_datetime LIMIT 100;
+EOF
+
+
+     (weight_trail_data || []).each do |weight|
+      current_date = weight['obs_datetime'].to_date
+      year = current_date.year
+
+      months = ActiveRecord::Base.connection.select_one <<EOF
+      SELECT timestampdiff(month, DATE('#{birthdate.to_date}'), DATE('#{current_date.to_date}')) AS `month`;
+EOF
+
+      month = months['month'].to_i
+      next if month > 58
+      begin
+        weight_trail[month] =  weight['value_numeric'].squish.to_f
+      rescue
+        next
+      end
+
+    end
+
+    months = ActiveRecord::Base.connection.select_one <<EOF
+    SELECT timestampdiff(month, DATE('#{birthdate.to_date}'), DATE('#{session_date.to_date}')) AS `month`;
+EOF
+
+    @sorted_weight_trail = []
+    (weight_trail || {}).sort_by{|x, y | x}.each do |m, weight|
+      @sorted_weight_trail << [m, weight.to_f]
+    end
+
+    age_in_months = (session_date.to_date.year * 12 + session_date.to_date.month) - (birthdate.year * 12 + birthdate.month)
+    sex = (person.gender == 'Male' || person.gender == 'M') ? 0 : 1
+    @weight_height_for_ages = {}
+
+    age_in_months += 5 if age_in_months < 53
+    weight_heights = WeightHeightForAge.find(:all,
+      :conditions => ["sex = ? AND age_in_months BETWEEN 0 AND ?", sex, age_in_months])
+    
+    (weight_heights || []).each do |data|
+
+      m = data.median_weight.to_f
+      l = data.standard_low_weight.to_f
+      h = data.standard_high_weight.to_f
+
+      @weight_height_for_ages[data.age_in_months] = {
+        :median_weight => m.round(2) ,
+        :standard_low_weight => (m - l).round(2),
+        :standard_high_weight => (m + h).round(2)
+      }
+    end
+
+    render :layout => false
+  end
+   
   def baby_chart
 
     @patient = Patient.find(params[:patient_id])
