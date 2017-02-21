27ebc4a5654801644af932f1a1c6c578a2354b46
Deliwe Nkhoma <dbnkhoma@gmail.com>
2017-02-21 10:05:13 +0200

    Changes to the Fuchia migration scripts by Pachawo and Comish

--- script/FUCHIA/encounters.rb
+++ script/FUCHIA/encounters.rb
@@ -1,7 +1,7 @@
 require 'fastercsv'
 User.current = User.find_by_username('admin')
 ScriptStared = Time.now()
-    
+
 Hiv_reception_encounter = EncounterType.find_by_name("Hiv reception")
 Vitals = EncounterType.find_by_name("Vitals")
 HIV_staging = EncounterType.find_by_name("HIV staging")
@@ -11,7 +11,9 @@ Encounter_id = Encounter.last
 Order_id = Order.last
 Appointment = EncounterType.find_by_name("APPOINTMENT")
 HIV_clinic_consultation = EncounterType.find_by_name("HIV CLINIC CONSULTATION")
-Parent_path = '/home/pachawo/Documents/msf'
+
+Source_path = '/home/deliwe/Desktop/Work/Fuchia/msf'
+Destination_path = '/home/deliwe/Desktop/Work/Fuchia/msf/msf_sql/'
 
 @@staging_conditions = []
 @@patient_visits = {}
@@ -23,11 +25,11 @@ def start
   encounter =  "INSERT INTO encounter (encounter_id, encounter_type, patient_id, provider_id, encounter_datetime, creator, uuid) VALUES "
   order = "INSERT INTO orders (order_id, order_type_id, orderer, concept_id, encounter_id, start_date, auto_expire_date, creator, date_created, patient_id, uuid) VALUES "
   drug_order = "INSERT INTO drug_order (order_id, drug_inventory_id, dose, equivalent_daily_dose, units, frequency, quantity) VALUES "
-  
-  `touch /home/pachawo/pats/encounters.sql && echo -n '#{encounter}' >> /home/pachawo/pats/encounters.sql` 
-  `touch /home/pachawo/pats/observations.sql && echo -n '#{obs}' >> /home/pachawo/pats/observations.sql`
-  `touch /home/pachawo/pats/orders.sql && echo -n '#{order}' >> /home/pachawo/pats/orders.sql`
-  `touch /home/pachawo/pats/drug_orders.sql && echo -n '#{drug_order}' >> /home/pachawo/pats/drug_orders.sql`
+
+  `touch #{Destination_path}encounters.sql && echo -n '#{encounter}' >> #{Destination_path}encounters.sql`
+  `touch #{Destination_path}observations.sql && echo -n '#{obs}' >> #{Destination_path}observations.sql`
+  `touch #{Destination_path}orders.sql && echo -n '#{order}' >> #{Destination_path}orders.sql`
+  `touch #{Destination_path}drug_orders.sql && echo -n '#{drug_order}' >> #{Destination_path}drug_orders.sql`
 
   if Encounter_id.blank?
     encounter_id = 1
@@ -41,7 +43,7 @@ def start
     order_id = Order_id.id.to_i + 1
   end
 
-  FasterCSV.foreach("#{Parent_path}/tb_follow_up.csv", :headers => true, :quote_char => '"', :col_sep => ',', :row_sep => :auto) do |row|
+  FasterCSV.foreach("#{Source_path}/tb_follow_up.csv", :headers => true, :quote_char => '"', :col_sep => ',', :row_sep => :auto) do |row|
     patient_id = row[3].to_i
     date_created = get_proper_date(row[9]).to_date rescue nil
     next_visit = get_proper_date(row[10]).to_date rescue nil
@@ -55,8 +57,8 @@ def start
       ############## code to log errors ########
       next
     end
-    encounter = Encounter.find(:all, :conditions => ["patient_id = ? and encounter_datetime 
-     between ? and ? and encounter_type = ?",patient_id, start_date, end_date, 
+    encounter = Encounter.find(:all, :conditions => ["patient_id = ? and encounter_datetime
+     between ? and ? and encounter_type = ?",patient_id, start_date, end_date,
      Hiv_reception_encounter.id])
 
     if encounter.blank?
@@ -71,14 +73,14 @@ def start
     weight = row[28].to_f rescue nil
     height = row[29].to_f rescue nil
     vitals_encounter = nil
-   
+
     puts ">>>#{encounter_id} ############# Vitals for #{patient_id}"
-    unless weight.blank? 
+    unless weight.blank?
       vitals_encounter = self.create_encounter(encounter_id, patient_id, Vitals.id, date_created)
       self.create_observation_value_numeric(vitals_encounter, "Weight (kg)", weight)
       encounter_id = encounter_id.to_i + 1
     end
-   
+
     unless height.blank?
       vitals_encounter = self.create_encounter(encounter_id, patient_id, Vitals.id, date_created) if vitals_encounter.blank?
       self.create_observation_value_numeric(vitals_encounter, "Height (cm)", height)
@@ -94,7 +96,7 @@ def start
     ############################ hiv staging and clinical consultation #########################
     s_c = @@patient_visits[ref_id] rescue []
 
-    hiv_staging_encounter = Encounter.find(:last, :conditions => ["patient_id = ? and encounter_datetime <= ? 
+    hiv_staging_encounter = Encounter.find(:last, :conditions => ["patient_id = ? and encounter_datetime <= ?
     and encounter_type = ?",patient_id, end_date, HIV_staging.id])
 
     if hiv_staging_encounter.blank?
@@ -105,19 +107,19 @@ def start
       encounter_id = encounter_id + 1
       (s_c || []).each do |cond|
         self.create_observation_value_coded(hiv_staging_encounter, "WHO Stage defining conditions not explicitly asked adult", cond)
-      end 
+      end
       encounter_id = encounter_id + 1
-    end 
+    end
 =begin
   if diagnostic_2.match(/preg/i) || diagnostic.match(/preg/i)
     self.create_observation_value_coded(hiv_staging_encounter, 'Pregnant', 'Yes')
-  end rescue nil 
+  end rescue nil
 =end
     if !cd4_count.blank? && !date_cd4_count.blank?
       self.create_observation_value_numeric(hiv_staging_encounter, 'CD4 count', cd4_count)
       self.create_observation_value_datetime(hiv_staging_encounter, 'Date of CD4 count', date_cd4_count)
     end rescue nil
-   
+
     puts ">>>#{encounter_id} ############# Clinic consultation for #{patient_id}"
 
     hiv_clinic_registration_encounter = self.create_encounter(encounter_id, patient_id, HIV_clinic_consultation.id, date_created)
@@ -125,7 +127,7 @@ def start
 
     if diagnostic_2.match(/preg/i) || diagnostic.match(/preg/i)
       self.create_observation_value_coded(hiv_clinic_registration_encounter, 'Pregnant', 'Yes')
-    end rescue nil 
+    end rescue nil
 
     if diagnostic_2.match(/NEUROPATH/i) || diagnostic.match(/NEUROPATH/i)
       self.create_observation_value_coded(hiv_clinic_registration_encounter,'MALAWI ART SIDE EFFECTS', 'Peripheral neuropathy')
@@ -173,22 +175,22 @@ def start
     encounter_id = encounter_id + 1
 
     (medication_on_this_visit || []).each do |medications|
-    
+
       (medications || []).each do |medication|
         medication = "Aspirin (300mg tablet)" if medication.match(/Unknown/i)
 
-        drug = Drug.find_by_name(medication) 
+        drug = Drug.find_by_name(medication)
 
         if next_visit.blank?
           n_visit = (date_created.to_date + 28.day).to_date
         else
           n_visit = next_visit
         end
-        puts "#####{medication}" 
+        puts "#####{medication}"
         drug_id = drug.id
 
         self.create_order(order_id, treatment_encounter, date_created, n_visit, drug_id)
-      
+
         weight_during_visit = PatientService.get_patient_attribute_value(patient, "current_weight", date_created)
         age_during_visit = (date_created.year - patient.person.birthdate.year).to_i
 
@@ -225,9 +227,9 @@ def start
           units = regimen_drug_order.units
         end
 =begin
-      moh_regimen_doses = MohRegimenDoses.find(:first, 
+      moh_regimen_doses = MohRegimenDoses.find(:first,
         :joins =>"INNER JOIN moh_regimen_ingredient i ON i.dose_id = moh_regimen_doses.dose_id",
-        :conditions => ["drug_inventory_id = ? AND #{weight_during_visit.to_f} >= FORMAT(min_weight,2) 
+        :conditions => ["drug_inventory_id = ? AND #{weight_during_visit.to_f} >= FORMAT(min_weight,2)
       AND #{weight_during_visit.to_f} <= FORMAT(max_weight,2)", drug.id])
 =end
         if dose.blank?
@@ -243,13 +245,28 @@ def start
         duration = (n_visit - date_created).to_i
         quantity = (duration * pills_per_day.to_i)
       # pill_given = DrugOrder.calculate_complete_pack(drug, quantity)
-      
+
         self.create_drug_order(order_id, drug_id, dose, pills_per_day, units, drug_frequency, quantity)
         self.create_observation_value_drug(dispensing_encounter, "Amount dispensed", drug_id, quantity, order_id)
         order_id = order_id + 1
-      # medication_pills_dispensed[drug.id] = pill_given 
-      
+      # medication_pills_dispensed[drug.id] = pill_given
+
       end
+=begin
+      if moh_regimen_doses.blank?
+        pills_per_day = 1.0
+      else
+        pills_per_day = (moh_regimen_doses.am.to_f + moh_regimen_doses.pm.to_f).to_f
+        puts "******************************* #{pills_per_day}"
+      end
+
+      duration = (date_created - n_visit).to_i
+      units = (duration * pills_per_day)
+      pill_given = DrugOrder.calculate_complete_pack(drug, units)
+
+      medication_pills_dispensed[drug.id] = pill_given
+      #puts "............. #{medication_pills_dispensed[drug.id]}"
+=end
     end
 
     ##########################################################################################
@@ -265,17 +282,17 @@ def start
 
   end
   puts "...............please wait............"
-  encounter_sql = File.read("/home/pachawo/pats/encounters.sql")[0...-1]
-  File.open("/home/pachawo/pats/encounters.sql", "w") {|sql| sql.puts encounter_sql << ";"}
+  encounter_sql = File.read("#{Destination_path}encounters.sql")[0...-1]
+  File.open("#{Destination_path}encounters.sql", "w") {|sql| sql.puts encounter_sql << ";"}
 
-  observation_sql = File.read("/home/pachawo/pats/observations.sql")[0...-1]
-  File.open("/home/pachawo/pats/observations.sql", "w") {|sql| sql.puts observation_sql << ";"}
+  observation_sql = File.read("#{Destination_path}observations.sql")[0...-1]
+  File.open("#{Destination_path}observations.sql", "w") {|sql| sql.puts observation_sql << ";"}
 
-  order_sql = File.read("/home/pachawo/pats/orders.sql")[0...-1]
-  File.open("/home/pachawo/pats/orders.sql", "w") {|sql| sql.puts order_sql << ";"}
+  order_sql = File.read("#{Destination_path}orders.sql")[0...-1]
+  File.open("#{Destination_path}orders.sql", "w") {|sql| sql.puts order_sql << ";"}
 
-  drug_order_sql = File.read("/home/pachawo/pats/drug_orders.sql")[0...-1]
-  File.open("/home/pachawo/pats/drug_orders.sql", "w") {|sql| sql.puts drug_order_sql << ";"}
+  drug_order_sql = File.read("#{Destination_path}drug_orders.sql")[0...-1]
+  File.open("#{Destination_path}drug_orders.sql", "w") {|sql| sql.puts drug_order_sql << ";"}
 
   puts "Script time: #{ScriptStared} - #{Time.now()}"
 end
@@ -305,7 +322,7 @@ end
 def self.create_hiv_reception(enc_id, patient_id, date_created)
   encounter = self.create_encounter(enc_id, patient_id, Hiv_reception_encounter.id, date_created)
   if !encounter.blank?
-    self.create_observation_value_coded(encounter,  "Guardian present", "No") 
+    self.create_observation_value_coded(encounter,  "Guardian present", "No")
     self.create_observation_value_coded(encounter, "Patient present", "Yes")
   end
 end
@@ -317,7 +334,7 @@ def self.create_encounter(encounter_id, patient_id, encounter_type_id, date_crea
     encounter.encounter_type = encounter_type_id
     encounter.patient_id = patient_id
     encounter.encounter_datetime = date_created.strftime("%Y-%m-%d 00:00:00")
-    
+
     uuid = ActiveRecord::Base.connection.select_one <<EOF
      select uuid();
 EOF
@@ -326,7 +343,7 @@ EOF
     insert_encounters = "(\"#{encounter_id}\",\"#{encounter_type_id}\",\"#{patient_id}\",\"#{User.current.id}\",\"#{date_created}\","
     insert_encounters += "\"#{User.current.id}\",\"#{uuid.values.first}\"),"
 
-    `echo -n '#{insert_encounters}' >> /home/pachawo/pats/encounters.sql`
+    `echo -n '#{insert_encounters}' >> #{Destination_path}encounters.sql`
   end
 
   return encounter
@@ -334,22 +351,22 @@ end
 
 def self.create_observation_value_numeric(encounter, concept_name, value)
     concept_id = ConceptName.find_by_name(concept_name).concept_id
-        
+
     uuid = ActiveRecord::Base.connection.select_one <<EOF
      select uuid();
 EOF
-    
+
     insert_observation = "(#{encounter.patient_id},#{encounter.id},#{concept_id},"
     insert_observation += "\"#{value}\",null,null,null,null,\"#{encounter.encounter_datetime.strftime("%Y-%m-%d 00:00:00")}\","
     insert_observation += "\"#{User.current.id}\",\"#{uuid.values.first}\"),"
 
-    `echo -n '#{insert_observation}' >> /home/pachawo/pats/observations.sql`
+    `echo -n '#{insert_observation}' >> #{Destination_path}observations.sql`
 end
 
 def self.create_observation_value_coded(encounter, concept_name, value_coded_concept_name)
     concept_id = ConceptName.find_by_name(concept_name).concept_id
     value_coded = ConceptName.find_by_name(value_coded_concept_name).concept_id
-    
+
     uuid =ActiveRecord::Base.connection.select_one <<EOF
      select uuid();
 EOF
@@ -357,7 +374,7 @@ EOF
     insert_observation_value_coded += "\"#{value_coded}\",null,null,null,\"#{encounter.encounter_datetime.strftime("%Y-%m-%d 00:00:00")}\","
     insert_observation_value_coded += "\"#{User.current.id}\",\"#{uuid.values.first}\"),"
 
-    `echo -n '#{insert_observation_value_coded}' >> /home/pachawo/pats/observations.sql`
+    `echo -n '#{insert_observation_value_coded}' >> #{Destination_path}observations.sql`
 end
 
 def self.create_observation_value_datetime(encounter, concept_name, date)
@@ -370,12 +387,12 @@ EOF
     insert_observation_value_datetime += "\"#{date.strftime("%Y-%m-%d %H:%M:%S")}\",null,null,\"#{encounter.encounter_datetime.strftime("%Y-%m-%d 00:00:00")}\","
     insert_observation_value_datetime += "\"#{User.current.id}\",\"#{uuid.values.first}\"),"
 
-    `echo -n '#{insert_observation_value_datetime}' >> /home/pachawo/pats/observations.sql`
+    `echo -n '#{insert_observation_value_datetime}' >> #{Destination_path}observations.sql`
 end
 
 def self.create_observation_value_drug(encounter, concept_name, value_drug, drug_amount, order_id)
     concept_id = ConceptName.find_by_name(concept_name).concept_id
-    
+
     uuid =ActiveRecord::Base.connection.select_one <<EOF
      select uuid();
 EOF
@@ -383,7 +400,7 @@ EOF
     insert_observation_value_coded += "null,null,#{value_drug},#{order_id},\"#{encounter.encounter_datetime.strftime("%Y-%m-%d 00:00:00")}\","
     insert_observation_value_coded += "\"#{User.current.id}\",\"#{uuid.values.first}\"),"
 
-    `echo -n '#{insert_observation_value_coded}' >> /home/pachawo/pats/observations.sql`
+    `echo -n '#{insert_observation_value_coded}' >> #{Destination_path}observations.sql`
 end
 
 def self.create_order(order_id, encounter, start_date, end_date, drug_id)
@@ -395,7 +412,7 @@ EOF
     insert_order += "\"#{end_date.strftime("%Y-%m-%d 00:00:00")}\",#{User.current.id},\"#{encounter.encounter_datetime.strftime("%Y-%m-%d 00:00:00")}\","
     insert_order += "#{encounter.patient_id},\"#{uuid.values.first}\"),"
 
-    `echo -n '#{insert_order}' >> /home/pachawo/pats/orders.sql`
+    `echo -n '#{insert_order}' >> #{Destination_path}orders.sql`
 
     return order_id
 end
@@ -404,7 +421,7 @@ def self.create_drug_order(order_id, drug_id, dose, pills_per_day, units, drug_f
 
     insert_drug_order = "(#{order_id},#{drug_id},#{dose},#{pills_per_day},\"#{units}\",\"#{drug_frequency}\",#{quantity}),"
 
-    `echo -n '#{insert_drug_order}' >> /home/pachawo/pats/drug_orders.sql`
+    `echo -n '#{insert_drug_order}' >> #{Destination_path}drug_orders.sql`
 end
 
 def get_proper_date (unfomatted_date)
@@ -459,7 +476,7 @@ def setup_staging_conditions
     	"HIV-associated nephropathy" => 'HIV associated nephropathy',
     	"Pneumocystis pneumonia" => 'Pneumocystis pneumonia',
     	"Angular cheilitis" => 'Angular cheilitis',
-    	"Isosporiasis" => 'Isosporiasis', 
+    	"Isosporiasis" => 'Isosporiasis',
     	"Seborrheic dermatitis" => 'Seborrhoeic dermatitis',
     	"Wart infection" => 'Wart virus infection',
     	"Papular pruritic eruption" => 'Papular pruritic eruptions',
@@ -473,16 +490,16 @@ def setup_staging_conditions
 
   conditions = []
 
-  FasterCSV.foreach("#{Parent_path}/TbFollowUpDiagnosis.csv", :headers => true, :quote_char => '"', :col_sep => ',', :row_sep => :auto) do |row|
+  FasterCSV.foreach("#{Source_path}/TbFollowUpDiagnosis.csv", :headers => true, :quote_char => '"', :col_sep => ',', :row_sep => :auto) do |row|
     conditions << row[4].to_i
     conditions = conditions.uniq
   end
 =begin
-  FasterCSV.foreach("#{Parent_path}/TbPatientDiagnosis.csv", :headers => true, :quote_char => '"', :col_sep => ',', :row_sep => :auto) do |row|
+  FasterCSV.foreach("#{Source_path}/TbPatientDiagnosis.csv", :headers => true, :quote_char => '"', :col_sep => ',', :row_sep => :auto) do |row|
     patient_id = row[3].to_i
     date_created = get_proper_date(row[1]).to_date rescue nil
-    next if date_created.blank? 
-    
+    next if date_created.blank?
+
     if @@patient_visits[patient_id].blank?
       @@patient_visits[patient_id] = {}
       @@patient_visits[patient_id][date_created] = []
@@ -491,17 +508,17 @@ def setup_staging_conditions
         @@patient_visits[patient_id][date_created] = []
       end
    end
-   @@patient_visits[patient_id][date_created] << @@conditions_map[get_references(Parent_path,row[4])]
+   @@patient_visits[patient_id][date_created] << @@conditions_map[get_references(Source_path,row[4])]
    @@patient_visits[patient_id][date_created] = @@patient_visits[patient_id][date_created].uniq
   end
 =end
-  FasterCSV.foreach("#{Parent_path}/TbFollowUpDiagnosis.csv", :headers => true, :quote_char => '"', :col_sep => ',', :row_sep => :auto) do |row|
+  FasterCSV.foreach("#{Source_path}/TbFollowUpDiagnosis.csv", :headers => true, :quote_char => '"', :col_sep => ',', :row_sep => :auto) do |row|
     follow_up_reference = row[3].to_i
 
     if @@patient_visits[follow_up_reference].blank?
       @@patient_visits[follow_up_reference] = []
     end
-    @@patient_visits[follow_up_reference] << @@conditions_map[get_references(Parent_path, row[4])]
+    @@patient_visits[follow_up_reference] << @@conditions_map[get_references(Source_path, row[4])]
     @@patient_visits[follow_up_reference] = @@patient_visits[follow_up_reference].uniq
   end
 end
@@ -514,7 +531,7 @@ def drug_mapping
     "Isoniazide prophylaxis" => ['INH or H (Isoniazid 100mg tablet)'],
     "FDC11 (TDF-3TC-EFV)" => ['TDF/3TC/EFV (300/300/600mg tablet)'],
     "FDC1 (D4T30-3TC-NVP)" => ['d4T/3TC (Stavudine Lamivudine 30/150 tablet)','NVP (Nevirapine 200 mg tablet)'],
-    "Lamivudine" => ['3TC (Lamivudine 150mg tablet)'], 
+    "Lamivudine" => ['3TC (Lamivudine 150mg tablet)'],
     "Stavudine (dosage unspecified)" => ['d4T (Stavudine 30mg tablet)'],
     "Nevirapine" => ['NVP (Nevirapine 200 mg tablet)'],
     "FDC2 pediatric (AZT-3TC-NVPp)" => ['AZT/3TC/NVP (60/30/50mg tablet)'],
@@ -561,10 +578,10 @@ def drug_mapping
     "NVP Single Dose + (AZT-3TC) regimen (Mother to child)" => ['AZT/3TC (Zidovudine and Lamivudine 300/150mg)','NVP (Nevirapine 200 mg tablet)']
   }
 
-  FasterCSV.foreach("#{Parent_path}/TbFollowUpDrug.csv", :headers => true, :quote_char => '"', :col_sep => ',', :row_sep => :auto) do |row|
+  FasterCSV.foreach("#{Source_path}/TbFollowUpDrug.csv", :headers => true, :quote_char => '"', :col_sep => ',', :row_sep => :auto) do |row|
     follow_up_ref = row[3].to_i
     date_created = get_proper_date(row[1]).to_date rescue nil
-    next if date_created.blank? 
+    next if date_created.blank?
 
     if @@drug_follow_up[follow_up_ref].blank?
       @@drug_follow_up[follow_up_ref] = []
@@ -579,7 +596,7 @@ end
 @@referenes = {}
 
 def set_referenes
-  FasterCSV.foreach("#{Parent_path}/TbReference.csv", :headers => true, :quote_char => '"', :col_sep => ',', :row_sep => :auto) do |row|
+  FasterCSV.foreach("#{Source_path}/TbReference.csv", :headers => true, :quote_char => '"', :col_sep => ',', :row_sep => :auto) do |row|
     @@referenes[row[0].to_i] = row[6]
    # puts ":: #{row[6]}"
   end
