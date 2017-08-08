65c3e652c436b582fc8d7bf4df3349c5feb8ca19
mangochiman <mangochiman@gmail.com>
2017-08-08 18:03:05 +0200

    DDE integration - updating demographics from DDE

--- lib/patient_service.rb
+++ lib/patient_service.rb
@@ -486,6 +486,87 @@ module PatientService
     patient.old_identification_number = data["patient"]["identifiers"]["National id"]
     patient
   end
+
+  def self.update_local_demographics_from_dde(person, data)
+    names = data["names"]
+    #identifiers = data["patient"]["identifiers"] rescue {}
+    addresses = data["addresses"]
+    attributes = data["attributes"]
+    birthdate = data["birthdate"]
+    birthdate_estimated = data["birthdate_estimated"]
+    gender = data["gender"]
+    
+    person_name = person.names[0]
+    person_address = person.addresses[0]
+
+
+    city_village = addresses["current_residence"] rescue nil
+    state_province = addresses["current_district"] rescue nil
+    neighborhood_cell = addresses["home_village"] rescue nil
+    county_district = addresses["home_ta"] rescue nil
+    address2 = addresses["home_district"] rescue nil
+    address1 = addresses["current_residence"] rescue nil
+
+    #person.gender = gender
+    #person.birthdate = birthdate.to_date
+    #person.birthdate_estimated = birthdate_estimated
+    #person.save
+
+    person.update_attributes({
+        :gender => gender,
+        :birthdate => birthdate.to_date,
+        :birthdate_estimated => birthdate_estimated
+      })
+
+    person_name.given_name = names["given_name"]
+    person_name.middle_name = names["middle_name"]
+    person_name.family_name = names["family_name"]
+    person_name.save
+
+    person_address.address1 = address1
+    person_address.address2 = address2
+    person_address.city_village = city_village
+    person_address.county_district = county_district
+    person_address.state_province = state_province
+    person_address.neighborhood_cell = neighborhood_cell
+    person_address.save
+
+    (attributes || {}).each do |key, value|
+      person_attribute_type = PersonAttributeType.find_by_name(key)
+      next if person_attribute_type.blank?
+      person_attribute_type_id = person_attribute_type.id
+      person_attrib = person.person_attributes.find_by_person_attribute_type_id(person_attribute_type_id)
+
+      if person_attrib.blank?
+        person_attrib = PersonAttribute.new
+        person_attrib.person_id = person.person_id
+        person_attrib.person_attribute_type_id = person_attribute_type_id
+      end
+
+      person_attrib.value = value
+      person_attrib.save
+    end
+=begin
+    #Leave this part commented out please!!. Do not update identifiers locally from DDE.
+   #New patient object was being created as a side effect when side National id was being updated
+    (identifiers || {}).each do |key, value|
+      patient_identifier_type = PatientIdentifierType.find_by_name(key)
+      next if patient_identifier_type.blank?
+      patient_identifier_type_id = patient_identifier_type.id
+      patient_ident = person.patient.patient_identifiers.find_by_identifier_type(patient_identifier_type_id)
+
+      if patient_ident.blank?
+        patient_ident = PatientIdentifier.new
+        patient_ident.patient_id = person.person_id
+        patient_ident.identifier_type = patient_identifier_type_id
+      end
+
+      patient_ident.identifier = value
+      patient_ident.save
+    end
+=end
+  end
+  
   ############# new DDE API END###################################
   
   def self.search_demographics_from_remote(params)
