8b5424515c8b6402500213d4aea68e543d5aad89
mangochiman <mangochiman@gmail.com>
2017-08-16 10:39:59 +0200

    Sending stock level data to couchdb

--- app/models/send_results_to_couchdb.rb
+++ app/models/send_results_to_couchdb.rb
@@ -8,12 +8,13 @@ class SendResultsToCouchdb < ActiveRecord::Base
     password = couchdb_details["source_password"]
     port = couchdb_details["source_port"]
     ip_address = couchdb_details["source_address"]
-    
+    doc_type = "StockLevelCouchdb"
+
     `curl -X PUT http://#{username}:#{password}@#{ip_address}:#{port}/#{database}`
-    `cd #{Rails.root}/db && curl -X PUT -d @couch_views.js http://#{username}:#{password}@#{ip_address}:#{port}/#{database}/_design/query`
+    `cd #{Rails.root}/db && curl -X PUT -d @couch_views.js http://#{username}:#{password}@#{ip_address}:#{port}/#{database}/_design/StockLevelCouchdb`
 
     key = data[:date].to_date.strftime("%Y/%m/%d")
-    info = JSON.parse(`curl -X GET http://#{username}:#{password}@#{ip_address}:#{port}/#{database}/_design/query/_view/by_date?key=\\\"#{key}\\\"`)
+    info = JSON.parse(`curl -X GET http://#{username}:#{password}@#{ip_address}:#{port}/#{database}/_design/#{doc_type}/_view/by_date?key=\\\"#{key}\\\"`)
     uuid = info['rows'].first['id'] rescue nil
     doc = JSON.parse(`curl -X GET http://#{username}:#{password}@#{ip_address}:#{port}/#{database}/#{uuid}`) rescue nil
 
@@ -26,10 +27,12 @@ class SendResultsToCouchdb < ActiveRecord::Base
       doc["receipts"] = data[:receipts]
       doc["supervision_verification"] = data[:supervision_verification]
       doc["supervision_verification_in_details"] = data[:supervision_verification_in_details]
+      doc["type"] = doc_type
 
       RestClient.post("http://#{username}:#{password}@#{ip_address}:#{port}/#{database}", doc.to_json, :content_type => "application/json")
     else
       url = "http://#{username}:#{password}@#{ip_address}:#{port}/#{database}/"
+      data["type"] = doc_type
       RestClient.post(url, data.to_json, :content_type => "application/json")
     end
 
