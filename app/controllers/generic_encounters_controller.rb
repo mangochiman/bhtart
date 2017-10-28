ca65cf474a99e7ad55ef124fc5db417174fd6e34
mangochiman <mangochiman@gmail.com>
2017-10-28 12:16:45 +0200

    Fast track fixes

--- app/controllers/generic_encounters_controller.rb
+++ app/controllers/generic_encounters_controller.rb
@@ -1774,7 +1774,6 @@ class GenericEncountersController < ApplicationController
     params[:observations] = observations
 
     create_obs(encounter ,  params)
-    url = next_task(encounter.patient)
 
     unless previous_hiv_clinic_consultation_observations.blank?
       session_date = session[:datetime].to_date rescue Date.today
@@ -1790,6 +1789,8 @@ class GenericEncountersController < ApplicationController
       end
     end
 
+    patient = Patient.find(patient_id)
+    url = next_task(patient)
     return url
   end
 
