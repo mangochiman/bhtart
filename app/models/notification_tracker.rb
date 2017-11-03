762a256956fbb15f3d103d87811e495b18758eba
mwatha <mwathabwanali@gmail.com>
2017-11-03 11:59:27 +0200

    adding trackers to alerts/notifications

---
+++ app/models/notification_tracker.rb
@@ -0,0 +1,14 @@
+class NotificationTracker < ActiveRecord::Base
+  set_table_name "notification_tracker"
+  set_primary_key "tracker_id"
+
+  belongs_to :user, :foreign_key => :user_id
+
+  def self.create_notification(name, response)
+    self.create(:notification_name => name,
+      :notification_response => response,
+      :notification_datetime => Time.now(),
+      :user_id => User.current.id)
+  end
+
+end
