c57a64f3c952a3525bc664f5ec2bdfc8b1b0e84b
mwatha <mwathabwanali@gmail.com>
2017-11-04 08:46:38 +0200

    adding trackers to alerts/notifications

--- app/controllers/notification_tracker_controller.rb
+++ app/controllers/notification_tracker_controller.rb
@@ -1,7 +1,8 @@
 class NotificationTrackerController < ApplicationController
   
   def track
-    notification = NotificationTracker.create_notification(params[:notification_name], params[:response_text])  
+    patient_id = session[:active_patient_id]
+    notification = NotificationTracker.create_notification(params[:notification_name], params[:response_text], patient_id)  
     render :text => notification.to_json and return
   end
 
--- app/models/notification_tracker.rb
+++ app/models/notification_tracker.rb
@@ -4,10 +4,11 @@ class NotificationTracker < ActiveRecord::Base
 
   belongs_to :user, :foreign_key => :user_id
 
-  def self.create_notification(name, response)
+  def self.create_notification(name, response, patient_id)
     self.create(:notification_name => name,
       :notification_response => response,
       :notification_datetime => Time.now(),
+      :patient_id => patient_id,
       :user_id => User.current.id)
   end
 
