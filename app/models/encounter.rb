0e06b510ed9da7e074541dfcf56ca8fc51b3bc55
mangochiman <mangochiman@gmail.com>
2017-12-06 14:01:23 +0200

    DHA fast track report

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -9,18 +9,18 @@ class Encounter < ActiveRecord::Base
   belongs_to :provider, :class_name => "Person", :foreign_key => :provider_id, :conditions => {:voided => 0}
   belongs_to :patient, :conditions => {:voided => 0}
 
-#  # TODO, this needs to account for current visit, which needs to account for possible retrospective entry
-#  named_scope :current, :conditions => 'DATE(encounter.encounter_datetime) = CURRENT_DATE()'
-#
-#  def before_save
-#    self.provider = User.current.person if self.provider.blank?
-#    # TODO, this needs to account for current visit, which needs to account for possible retrospective entry
-#    self.encounter_datetime = Time.now if self.encounter_datetime.blank?
-#  end
-#
-#  def after_save
-#    self.add_location_obs
-#  end
+  # TODO, this needs to account for current visit, which needs to account for possible retrospective entry
+  named_scope :current, :conditions => 'DATE(encounter.encounter_datetime) = CURRENT_DATE()'
+
+  def before_save
+    self.provider = User.current.person if self.provider.blank?
+    # TODO, this needs to account for current visit, which needs to account for possible retrospective entry
+    self.encounter_datetime = Time.now if self.encounter_datetime.blank?
+  end
+
+  def after_save
+    self.add_location_obs
+  end
 
   def after_void(reason = nil)
     unless self.name.upcase == 'ART ADHERENCE'
@@ -63,7 +63,7 @@ EOF
       weight = observations.select {|obs| obs.concept.concept_names.map(&:name).include?("WEIGHT (KG)") || obs.concept.concept_names.map(&:name).include?("Weight (kg)") && "#{obs.answer_string}".upcase != '0.0' }
       height = observations.select {|obs| obs.concept.concept_names.map(&:name).include?("HEIGHT (CM)") || obs.concept.concept_names.map(&:name).include?("Height (cm)") && "#{obs.answer_string}".upcase != '0.0' }
       vitals = [weight_str = weight.first.answer_string + 'KG' rescue 'UNKNOWN WEIGHT',
-                height_str = height.first.answer_string + 'CM' rescue 'UNKNOWN HEIGHT']
+        height_str = height.first.answer_string + 'CM' rescue 'UNKNOWN HEIGHT']
       temp_str = temp.first.answer_string + '°C' rescue nil
       vitals << temp_str if temp_str                          
       vitals.join(', ')
@@ -78,9 +78,9 @@ EOF
     encounter_types_hash = encounter_types.inject({}) {|result, row| result[row.encounter_type_id] = row.name; result }
     with_scope(:find => opts) do
       rows = self.all(
-         :select => 'count(*) as number, encounter_type', 
-         :group => 'encounter.encounter_type',
-         :conditions => ['encounter_type IN (?)', encounter_types.map(&:encounter_type_id)]) 
+        :select => 'count(*) as number, encounter_type',
+        :group => 'encounter.encounter_type',
+        :conditions => ['encounter_type IN (?)', encounter_types.map(&:encounter_type_id)])
       return rows.inject({}) {|result, row| result[encounter_types_hash[row['encounter_type']]] = row['number']; result }
     end     
   end
